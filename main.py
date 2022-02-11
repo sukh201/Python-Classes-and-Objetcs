a85f8ee876da11934b732f6c9d50aa6262353b8b
Sukh Bal <sukh770@gmail.com>
2022-02-11 07:24:59 -0800

    Adding main and user files
    Adding main and user file for Python project.

---
+++ main.py
@@ -0,0 +1,7 @@
+from user import User
+
+app_user_one = User('abc@gmail.com', 'John Doe', '1234', 'Software Developer')
+app_user_one.get_user_info()
+
+app_user_one = User('xyz@yahoo.com', 'Dave John', '1234', 'Python Developer')
+app_user_one.get_user_info()
